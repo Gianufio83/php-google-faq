9e871df0cb0398e706031e9647364be2a236abec
Gianufio83 <fiore.gianuario@gmail.com>
2020-02-24 14:49:21 +0100

    inizio struttura

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" href="css/style.css">
+   <title>Faq</title>
+ </head>
+ <body>
+   <div class="first">
+     <img src="img/googlelogo.png" alt="">
+     <p>Privacy e Termini</p>
+   </div>
+   <div class="container">
+     <div class="left">
+       <ul >
+         <li>Introduzione</li>
+         <li>Norme sulla privacy</li>
+         <li>Termini di servizio</li>
+         <li>Tecnologie</li>
+         <li><a href="#">Domande frequenti</a></li>
+       </ul>
+     </div>
+     <div class="right">
+       <ul>
+         <li>Account Google</li>
+       </ul>
+     </div>
+   </div>
+ </body>
+ </html>
